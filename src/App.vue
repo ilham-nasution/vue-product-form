4e3f09433d2c85967d18706b0b62e1c0e60daf76
Ilham Rahman <ilhaamrahman@gmail.com>
2023-04-13 12:06:18 +0700

    create form input component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from "vue";
 import useSupabase from "./composables/useSupabase";
 import type BrandInterface from "./types/BrandInterface";
 import type FormInterface from "./types/FormInterface";
+import FormInput from "./components/FormInput.vue";
 
 const brands = ref<BrandInterface[] | null>([]);
 const forms = ref<FormInterface[] | null>([]);
@@ -31,44 +32,11 @@ onMounted(() => {
 
     <form class="mt-5">
       <div v-for="form in forms" :key="form.id" class="mb-2">
-        <label v-if="form.form_types?.name === 'textarea'" class="block">
-          <span>Description</span>
-          <textarea class="mt-1 block w-full" rows="3" />
-        </label>
-
-        <label v-else-if="form.form_types?.name === 'select'" class="block">
-          <span>Brand</span>
-          <select class="block w-full mt-1">
-            <option v-for="brand in brands" :key="brand.id">
-              {{ brand.name }}
-            </option>
-          </select>
-        </label>
-
-        <div v-else-if="form.form_types?.name === 'radio'">
-          <p>{{ form.label }}</p>
-          <label class="inline-flex items-center">
-            <input type="radio" name="condition" />
-            <span class="mx-2">New</span>
-          </label>
-          <label class="inline-flex items-center">
-            <input type="radio" name="condition" />
-            <span class="mx-2">Old</span>
-          </label>
-        </div>
-
-        <label
-          v-else-if="form.form_types?.name === 'checkbox'"
-          class="inline-flex items-center"
-        >
-          <input type="checkbox" />
-          <span class="ml-2">I agree to the terms and conditions</span>
-        </label>
-
-        <label v-else class="block">
-          <span>{{ form.label }}</span>
-          <input :type="form.form_types?.name" class="mt-1 block w-full" />
-        </label>
+        <FormInput
+          :label="form.label"
+          :type="form.form_types?.name"
+          :brands="brands"
+        />
       </div>
 
       <div class="text-center">
---
+++ src/components/FormInput.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type BrandInterface from "./types/BrandInterface";
+
+defineProps<{
+  label?: string;
+  type?: string;
+  brands?: BrandInterface[] | null;
+}>();
+</script>
+
+<template>
+  <label v-if="type === 'textarea'" class="block">
+    <span>Description</span>
+    <textarea class="mt-1 block w-full" rows="3" />
+  </label>
+
+  <label v-else-if="type === 'select'" class="block">
+    <span>Brand</span>
+    <select class="block w-full mt-1">
+      <option v-for="brand in brands" :key="brand.id">
+        {{ brand.name }}
+      </option>
+    </select>
+  </label>
+
+  <div v-else-if="type === 'radio'">
+    <p>{{ label }}</p>
+    <label class="inline-flex items-center">
+      <input type="radio" name="condition" />
+      <span class="mx-2">New</span>
+    </label>
+    <label class="inline-flex items-center">
+      <input type="radio" name="condition" />
+      <span class="mx-2">Old</span>
+    </label>
+  </div>
+
+  <label v-else-if="type === 'checkbox'" class="inline-flex items-center">
+    <input type="checkbox" />
+    <span class="ml-2">I agree to the terms and conditions</span>
+  </label>
+
+  <label v-else class="block">
+    <span>{{ label }}</span>
+    <input :type="type" class="mt-1 block w-full" />
+  </label>
+</template>
