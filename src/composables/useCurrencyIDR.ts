cbd64415f2620aa3a4dc7c39c8877c01b5b307a1
Ilham Rahman <ilhaamrahman@gmail.com>
2023-04-14 10:50:02 +0700

    fix types

--- src/composables/useCurrencyIDR.ts
+++ src/composables/useCurrencyIDR.ts
@@ -1,7 +1,7 @@
-export default function (price) {
+export default function (price: number | null) {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
     minimumFractionDigits: 0,
-  }).format(price);
+  }).format(price ? price : 0);
 }
